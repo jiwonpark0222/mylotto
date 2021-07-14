cfe8fe7fbf5bf8e62ba7d3111b2d748cd711e0fa
박지원 <jiwon.park@motiv-i.com>
2021-07-14 17:24:11 +0900

    test

--- app/src/main/java/com/lotto/mylotto/ResultActivity.kt
+++ app/src/main/java/com/lotto/mylotto/ResultActivity.kt
@@ -2,10 +2,19 @@ package com.lotto.mylotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lotto.mylotto.databinding.ActivityConstellationBinding
 
 class ResultActivity : AppCompatActivity(){
+
+    private lateinit var resultBinding: ActivityConstellationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+
+        resultBinding = ActivityConstellationBinding.inflate(layoutInflater)
+        val view = resultBinding.root
+
+        setContentView(view)
+
     }
 }
