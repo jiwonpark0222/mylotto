85155128f49039a94f9b53f14bc5624c431e4043
박지원 <jiwon.park@motiv-i.com>
2021-07-14 17:15:24 +0900

    init commit

---
+++ app/src/main/java/com/lotto/mylotto/ConstellationActivity.kt
@@ -0,0 +1,11 @@
+package com.lotto.mylotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstellationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constellation)
+    }
+}
---
+++ app/src/main/java/com/lotto/mylotto/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.lotto.mylotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
